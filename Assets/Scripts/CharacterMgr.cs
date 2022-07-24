4a313653c4f058c53974d208cfd81bbbf93e2c95
dohyun <0812kdh@naver.com>
2022-07-24 16:55:31 +0900

    HP,턴

--- Assets/Scripts/CharacterMgr.cs
+++ Assets/Scripts/CharacterMgr.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterMgr : MonoBehaviour
 {
+    public Text playerHPText;//플레이어 HP텍스트
+    public Text playerEnergeText;//플레이어 에너지 텍스트
+
+    public int playerFullHP = 30000;//플레이어 총 HP;
+    public int playerHP;//플레이어 HP
+    public int playerFullEnerge = 90;//플레이어 에너지
+    public int playerEnerge;//플레이어 에너지
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerHP = playerFullHP;//처음엔 풀피
+        playerEnerge = playerFullEnerge;//처음엔 풀 에너지
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerHP -= 1;//테스트 코드
+        PlayerHPManage();
+        PlayerEnergeManage();
+    }
+    void PlayerHPManage()
+    {
+        playerHPText.text = "HP : "+playerHP.ToString("0");//화면에 보이게
+    }
+    void PlayerEnergeManage()
+    {
+        playerEnergeText.text = playerEnerge.ToString("0");//화면에 보이게
     }
 }
---
+++ Assets/Scripts/Turn.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Turn : MonoBehaviour
+{
+    public Text test;
+    public int turnNumber = 1;
+
+
+    void Start()
+    {
+        test.text = "시작";
+        battle();
+    }
+
+    void battle()
+    {
+        switch(turnNumber)
+        {
+            case 1:
+                Invoke("player1", 2);
+                break;
+            case 2:
+                Invoke("player2", 2);
+                break;
+            case 3:
+                Invoke("player3", 2);
+                break;
+            case 4:
+                Invoke("player4", 2);
+                break;
+            case 5:
+                Invoke("boss", 2);
+                break;
+            default:
+                test.text = "버그발생";
+                break;
+        }
+    }
+
+    void player1()
+    {
+        turnNumber += 1;
+        test.text = "1";
+        battle();
+    }
+
+    void player2()
+    {
+        turnNumber += 1;
+        test.text = "2";
+        battle();
+    }
+
+    void player3()
+    {
+        turnNumber += 1;
+        test.text = "3";
+        battle();
+    }
+
+    void player4()
+    {
+        turnNumber += 1;
+        test.text = "4";
+        battle();
+    }
+
+    void boss()
+    {
+        turnNumber += 1;
+        test.text = "5";
+        battle();
+    }
+
+}
