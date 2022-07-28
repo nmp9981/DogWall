003686b74a555cf1bc3530368c317a6d67d89a5c
yeongchankim <b611040@g.hongik.ac.kr>
2022-07-28 22:19:39 +0900

    팀 선택 가능하게 수정, 저장은 아직, 유닛 강화랑 유닛 선택 분리

--- Assets/Scripts/UI_Manager.cs
+++ Assets/Scripts/UI_Manager.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class UI_Manager : MonoBehaviour
 {
     public int level1 = 0, level2 = 0;
     public int x = 0, y = 0;
     Data_Manager data;
+    private bool upgrade = false;
     void Awake()
     {
         data = GameObject.Find("Data_Manager").GetComponent<Data_Manager>();
@@ -25,7 +27,14 @@ public class UI_Manager : MonoBehaviour
    
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Character_ADD("Monster Dummy/000","질퍽이",10,10,"몰루?");
+            Character_ADD("Monster Dummy/111","꼬북이",10,10,"물");
+            Character_ADD("Monster Dummy/121","잉어킹",10,10,"물");
+            Character_ADD("Monster Dummy/212","고라파덕",10,10,"물");
+            Character_ADD("Monster Dummy/333","뭐더라",10,10,"어둠");
+        }
     }
     public void UI_LEVEL1_Controll(int level)
     {
@@ -50,14 +59,22 @@ public class UI_Manager : MonoBehaviour
                 switch(level2)
                 {
                     case 0:
-                        UI("Team");
+                        UI("Team");//팀
                         break;
                     case 1:
-                        UI("Team","Teams");
+                        UI("Team","Teams");//팀편성버튼
+                        upgrade = false;
                         break;
                     case 2:
-                        UI("Team","Unit_Assign");
+                        UI("Team","Unit_Select");//선택 버튼
+                        Unit_Select_Change();
                         break;
+                    case 3:
+                        UI("Team","Unit_Select");//유닛 강화버튼
+                        upgrade = true;
+                        Unit_Select_Change();
+                        break;
+                    
                 }
                 break;
         }
@@ -105,12 +122,97 @@ public class UI_Manager : MonoBehaviour
         int i = data.Get_Character_Count();
         x = (i-1) % 4;
         y = (i - x) / 4;
+        
+        Transform parent = GameObject.Find("Canvas").transform.Find("Team").transform.Find("Unit_Select").transform.Find("Scroll View").transform.Find("Viewport").transform.Find("Content").gameObject.GetComponent<Transform>();
+        if(y>=4)
+            parent.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,1000 + (y-3)*300);
         GameObject temp = Instantiate(Resources.Load<GameObject>("Prefabs/Ch"));
-        Transform parent = GameObject.Find("Canvas").transform.Find("Team").transform.Find("Unit_Assign").transform.Find("Scroll View").transform.Find("Viewport").transform.Find("Content").gameObject.GetComponent<Transform>();
         temp.transform.SetParent(parent);
-        Vector3 pos = new Vector3(43 + (x * 259f), 395 - (y * 253));
+        Vector3 pos = new Vector3(43 + (x * 259f), 1395 - (y * 253));
         temp.transform.position = pos;
         temp.name = data.list[i-1].Name;
         temp.GetComponent<Image>().sprite = data.list[i-1].Img;//보여주기식용, 나중에 리스트 먼저 나오면 수정해야함
+        temp.transform.GetComponent<Button>().onClick.AddListener(Unit_Choose);
+    }
+
+    void Unit_Select_Change()
+    {
+        GameObject panel = GameObject.Find("Canvas").transform.Find("Team").transform.Find("Unit_Select").gameObject;
+        if(upgrade == true)
+        {
+            panel.transform.Find("Text").GetComponent<Text>().text = "강화할 유닛 선택"; 
+        }
+        else
+        {
+            GameObject character = EventSystem.current.currentSelectedGameObject;
+            string idx = character.name.Substring(character.name.Length-1);
+            switch(idx)
+            {
+                case "1":
+                    idx = "첫 ";
+                    break;
+                case "2":
+                    idx = "두 ";
+                    break;
+                case "3":
+                    idx = "세 ";
+                    break;
+                case "4":
+                    idx = "네 ";
+                    break;
+            }
+            panel.transform.Find("Text").GetComponent<Text>().text = idx + "번째 유닛 선택";
+        }
+    }
+
+    void Unit_Choose()
+    {
+        GameObject panel = GameObject.Find("Canvas").transform.Find("Team").transform.Find("Unit_Select").gameObject;
+        string idx = panel.transform.Find("Text").gameObject.GetComponent<Text>().text.Substring(0,1);
+        GameObject target;
+        switch(idx)
+        {
+            case "첫":
+                idx = "1";
+                break;
+            case "두":
+                idx = "2";
+                break;
+            case "세":
+                idx = "3";
+                break;
+            case "네":
+                idx = "4";
+                break;
+        }
+        target = GameObject.Find("Canvas").transform.Find("Team").transform.Find("Teams").transform.Find("Character" + idx).gameObject;
+        GameObject Unit = EventSystem.current.currentSelectedGameObject;
+        for(int i = 0; i < data.list.Count; i++)
+        {
+            if(Unit.name == data.list[i].Name)
+            {
+                target.transform.GetChild(0).GetComponent<Image>().sprite = data.list[i].Img;
+                target.transform.GetChild(1).GetComponent<Text>().text = data.list[i].HP.ToString();
+                target.transform.GetChild(2).GetComponent<Text>().text = data.list[i].ATK.ToString();
+                target.transform.GetChild(3).GetComponent<Text>().text = data.list[i].Type;
+                UI_LEVEL2_Controll(1);
+                break;
+            }
+        }
+    }
+
+
+    public void Back_for_Select()
+    {
+        if(!upgrade)
+        {
+            UI_LEVEL1_Controll(1);
+            UI_LEVEL2_Controll(1);
+        }
+        else
+        {
+            UI_LEVEL1_Controll(1);
+            UI_LEVEL2_Controll(0);
+        }
     }
 }
