3c94168715161fb424390ec4767ae7f749eb55d1
yeongchankim <b611040@g.hongik.ac.kr>
2022-08-12 23:40:07 +0900

    엑셀을 바로 받는 거는 아마 없는 듯?? 특정 프로그램을 통해서 엑셀 파일을 json파일로 변환하는 과정은 필요함 -> csv파일로 저장하여 받는 방법으로 바꿈 CSV_READER_TEST.cs 내부 Read함수 수정해야함

---
+++ Assets/Scripts/Data/CSV_Reader_Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSV_Reader_Test : MonoBehaviour
+{
+    //테스팅용, Read할 때 csv파일을 메모장으로 열어서 UTF-8로 다시 인코딩해줘야 한글 깨지지 않음!!
+
+    //CSV_READER스크립트에 경로를 Resources폴더의 Data폴더 안의 특정 파일을 읽도록 설정함!! Read안에 파일의 이름을 string으로 적어주면 됨!!
+
+    //Read함수를 이용하여 saveData클래스에 한번에 저장할 계획, 현재 캐릭터 클래스나 몬스터 클래스에 맞도록 데이터셋 생성 필요!!
+    void Start()
+    {
+        Read("CSV_TEST");
+    }
+
+    void Read(string file)
+    {
+        List<Dictionary<string, object>> data = CSV_Reader.Read(file);
+
+        for(var i = 0; i < data.Count; i++)
+        {
+            Debug.Log("이름 = " + data[i]["이름"] + ", HP = " + data[i]["HP"].ToString() + ", 공격력 = " + data[i]["공격력"]);
+        }
+    }
+}
