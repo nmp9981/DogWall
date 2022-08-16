9abfa3d80ee18920e7a710d44c454af47b2985c1
nmp9981 <tybnasgo@naver.com>
2022-08-16 19:31:09 +0900

    퀘스트DB추가

--- Assets/Scripts/Data_Manager.cs
+++ Assets/Scripts/Data_Manager.cs
@@ -9,6 +9,8 @@ public class Data_Manager : MonoBehaviour
     public List<CharacterDataClass> Characterlist = new List<CharacterDataClass>();
     public List<SkillDataClass> SkillList = new List<SkillDataClass>();
     public List<MonsterDataClass> MonsterList = new List<MonsterDataClass>();
+    public List<QuestDataClass> QuestList = new List<QuestDataClass>();
+
     void Start()
     {
 
@@ -32,5 +34,9 @@ public class Data_Manager : MonoBehaviour
     public void Add_Skill(string name, int energe, string explain,int attack,int decrease,int healHP,bool provocation,bool action,int healE,int blood,int trunCount,int rare)
     {
         SkillList.Add(new SkillDataClass(name,energe,explain,attack,decrease,healHP,provocation,action,healE,blood,trunCount,rare));
+    }
+    public void Add_Quest(int episode,int world,int bigStage,int middleStage,int smallStage,int mobIndex,int mobAction)
+    {
+        QuestList.Add(new QuestDataClass(episode, world, bigStage, middleStage, smallStage, mobIndex, mobAction));
     }
 }
