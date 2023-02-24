29b719b49a8296ed591c122a91bc10726a99bec3
Yeongchan <b611040@gmail.com>
2023-02-24 16:17:58 +0900

    epilog 입력 버전 추가, 로딩씬 스크립트만 추가 내용 구현 x, 씬구현 x

---
+++ Assets/Scripts/Home/Story/LoadingScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadingScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public static void SceneLoad(string dst)
+    {
+        SceneManager.LoadScene(dst);
+    }
+}
---
+++ Assets/Scripts/Home/Story/StoryData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Epilog", menuName = "EpilogData", order = 1)]
+public class StoryData : ScriptableObject
+{
+    public List<string> Name { get { return _name; } set { _name = value; } }
+    public List<Sprite> Img { get { return _img; } set { _img = value; } }
+    public string Input { get { return _input; } set { _input = value; } }
+
+    [Tooltip("캐릭터 이름")]
+    [SerializeField] private List<string> _name;
+    [Tooltip("캐릭터 이미지")]
+    [SerializeField] private List<Sprite> _img;
+
+    [Tooltip("스토리 이름")]
+    [SerializeField] private string _input;
+
+}
