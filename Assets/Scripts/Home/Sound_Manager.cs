6390373b31098ce201c0e327201d6a8560ffbd56
catsnakedog <0812kdh@gmail.com>
2023-05-13 12:37:36 +0900

    사운드 사용방법 sound.Play("플레이하고 싶은 사운드의 이름"); BGM, SFX는 따로 적어줄 필요없이 그냥 해당하는 사운드 이름만 play에 스트링으로 전달해주시면 됩니다 사운드를 추가하고 싶으시면 Resources/Sound 안에 BGM, SFX 구별해서 mp3파일 넣어두시고 Sound_Manager에 BGM, SFX enum에 넣어두신 사운드 파일의 이름만 추가해주시면 됩니다

--- Assets/Scripts/Home/Sound_Manager.cs
+++ Assets/Scripts/Home/Sound_Manager.cs
@@ -3,19 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
+using System.CodeDom.Compiler;
+using System;
+
 public class Sound_Manager : MonoBehaviour
 {
     // Start is called before the first frame update
     public static Sound_Manager sound;
     private DataManager Data;
     public AudioMixer master;
-    public Slider Bgm,Sfx;
+    public Slider Bgm, Sfx;
 
-    public AudioClip[] BGMList;
-    public AudioClip[] SFXList;
+    AudioClip[] BGMs = new AudioClip[(int)BGM.MaxCount];
+    AudioClip[] SFXs = new AudioClip[(int)SFX.MaxCount];
+
+    HashSet<string> SFXNames =  new HashSet<string> ();
+    HashSet<string> BGMNames = new HashSet<string>();
+
+    AudioSource BGMSource;
+    AudioSource SFXSource;
+
+    enum BGM
+    {
+        엠피리언_전투BGM_2,
+        월령국_전투BGM_최종,
+        제한구역_변주,
+        제한구역,
+        홈화면_BGM,
+        MaxCount
+    }
+
+    enum SFX
+    {
+        Back_button,
+        Back_to_home,
+        Button,
+        Clear,
+        Game_over,
+        Quest_entry,
+        Reinforce,
+        MaxCount
+    }
+
     void Awake()
     {
-        if(sound == null)
+        if (sound == null)
         {
             sound = this;
             DontDestroyOnLoad(gameObject);
@@ -32,67 +64,67 @@ public class Sound_Manager : MonoBehaviour
         Find_Sliders();
         Load_Value();
         SoundPooling();
+        SetAudioSource();
+        Play("홈화면_BGM"); // 테스트용 BGM
+        Play("Clear"); // 테스트용 SFX
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void Find_Sliders()
     {
-        if(Bgm == null || Sfx == null)
+        if (Bgm == null || Sfx == null)
         {
             GameObject Canvas = GameObject.Find("Canvas");
             GameObject Setting = null;
-            for(int i = 0; i < Canvas.transform.childCount; i++)
+            for (int i = 0; i < Canvas.transform.childCount; i++)
             {
-                if(Canvas.transform.GetChild(i).name == "Setting")
+                if (Canvas.transform.GetChild(i).name == "Setting")
                 {
                     Setting = Canvas.transform.GetChild(i).gameObject;
                     break;
                 }
             }
-            if(Setting == null)
+            if (Setting == null)
             {
                 Debug.Log("Setting패널이 존재하지 않음");
             }
             else
             {
-                for(int i = 0; i < Setting.transform.childCount; i++)
+                for (int i = 0; i < Setting.transform.childCount; i++)
                 {
-                    if(Setting.transform.GetChild(i).name == "SFX_Slider")
+                    if (Setting.transform.GetChild(i).name == "SFX_Slider")
                         Sfx = Setting.transform.GetChild(i).GetComponent<Slider>();
-                    else if(Setting.transform.GetChild(i).name == "BGM_Slider")
+                    else if (Setting.transform.GetChild(i).name == "BGM_Slider")
                         Bgm = Setting.transform.GetChild(i).GetComponent<Slider>();
-                    if(Bgm != null && Sfx != null)
+                    if (Bgm != null && Sfx != null)
                         break;
                 }
             }
-            
+
         }
     }
     public void Sound_Control()
     {
-        if(Bgm.value == -40)
+        master.SetFloat("BGM", Bgm.value);
+        master.SetFloat("SFX", Sfx.value);
+        if (Bgm.value == -40)
         {
-            master.SetFloat("BGM",-80);
+            master.SetFloat("BGM", -80);
         }
-        else if(Sfx.value == -40)
+        if (Sfx.value == -40)
         {
-            master.SetFloat("SFX",-80);
+            master.SetFloat("SFX", -80);
         }
-        else
-        {
-            master.SetFloat("BGM",Bgm.value);
-            master.SetFloat("SFX",Sfx.value);
-        }
-        
+
     }
     public void Load_Value()
     {
-        master.SetFloat("BGM",Data.saveData.ui.bgm);
-        master.SetFloat("SFX",Data.saveData.ui.sfx);
+        master.SetFloat("BGM", Data.saveData.ui.bgm);
+        master.SetFloat("SFX", Data.saveData.ui.sfx);
         Bgm.value = Data.saveData.ui.bgm;
         Sfx.value = Data.saveData.ui.sfx;
     }
@@ -104,7 +136,49 @@ public class Sound_Manager : MonoBehaviour
 
     void SoundPooling()
     {
-        BGMList = Resources.LoadAll<AudioClip>("Sound/BGM");
-        SFXList = Resources.LoadAll<AudioClip>("Sound/SFX");
+        string[] BGMNames = System.Enum.GetNames(typeof(BGM));
+        string[] SFXNames = System.Enum.GetNames(typeof(SFX));
+        for (int i = 0; i < BGMNames.Length - 1; i++)
+        {
+            BGMs[i] = Resources.Load<AudioClip>("sound/BGM/" + BGMNames[i]);
+            this.BGMNames.Add(BGMNames[i]);
+        }
+        for (int i = 0; i < SFXNames.Length - 1; i++)
+        {
+            SFXs[i] = Resources.Load<AudioClip>("sound/SFX/" + SFXNames[i]);
+            this.SFXNames.Add(SFXNames[i]);
+        }
+    }
+
+    void SetAudioSource()
+    {
+        AudioSource[] temp;
+        temp = gameObject.GetComponents<AudioSource>();
+        BGMSource = temp[0];
+        SFXSource = temp[1];
+        BGMSource.loop = true;
+    }
+
+    public void Play(string soundName, float pitch = 1.0f)
+    {
+        if (BGMNames.Contains(soundName))
+        {
+            if(BGMSource.isPlaying)
+            {
+                BGMSource.Stop();
+            }
+            BGMSource.pitch = pitch;
+            BGMSource.clip = BGMs[(int)((BGM)Enum.Parse(typeof(BGM), soundName))];
+            BGMSource.Play();
+        }
+        else if(SFXNames.Contains(soundName))
+        {
+            SFXSource.pitch = pitch;
+            SFXSource.PlayOneShot(SFXs[(int)((SFX)Enum.Parse(typeof(SFX), soundName))]);
+        }
+        else
+        {
+            Debug.Log("해당 사운드는 존재하지 않습니다");
+        }
     }
 }
